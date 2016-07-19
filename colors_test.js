c0a02cc1446a09e88cfefa5bad0455f20580a3cf
wangyanqiang <wangyanqiang@tvmining.com>
2016-07-19 11:36:49 +0800

    update

---
+++ colors_test.js
@@ -0,0 +1,11 @@
+/**
+ * Created by wyq on 2016/7/18.
+ */
+
+var colors = require("colors/safe");
+
+console.log(colors.red.underline('i like cake and pies'));
+
+
+
+
---
+++ ztestF.js
@@ -0,0 +1,8 @@
+var prizeNameMap = {
+	1: "实物",
+	3: "URL",
+	9: "优惠券",
+	105: "红包"
+};
+
+console.log(prizeNameMap[105]);
