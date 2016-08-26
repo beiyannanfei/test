ff5010d7511b8a16d80913fcfb7a02b5d18d35c4
yanqiang.wang <wangyanqiang@sensoro.com>
2016-08-26 16:49:40 +0800

    add sinon test

--- SENSORO/test/mocha_test.js
+++ SENSORO/test/mocha_test.js
@@ -27,6 +27,82 @@ describe("test sinon func", function () {
 			});
 		});
 	});
+
+	context("sinon normal ok", function () {
+		before(function () {
+			console.log("sinon normal before ok");
+			mySinon.stopStub();
+			mySinon.startStub();
+		});
+		after(function () {
+			console.log("sinon normal after ok");
+			mySinon.stopStub();
+		});
+		it("startStub ok", function (done) {
+			myFunc.test(0, function (err, val) {
+				console.log(arguments);
+				val.should.equal("startStub ok");
+				return done();
+			});
+		});
+	});
+
+	context("sinon normal err", function () {
+		before(function () {
+			console.log("sinon normal before err");
+			mySinon.stopStub();
+			mySinon.startStub();
+		});
+		after(function () {
+			console.log("sinon normal after err");
+			mySinon.stopStub();
+		});
+		it("startStub err", function (done) {
+			myFunc.test(1, function (err) {
+				console.log(arguments);
+				err.should.equal("startStub err");
+				return done();
+			});
+		});
+	});
+
+	context("sinon always ok", function () {
+		before(function () {
+			console.log("sinon always ok before");
+			mySinon.stopStub();
+			mySinon.okStub();
+		});
+		after(function () {
+			console.log("sinon always ok after");
+			mySinon.stopStub();
+		});
+		it("okStub", function (done) {
+			myFunc.test(1, function (err, val) {
+				console.log(arguments);
+				val.should.equal("okStub");
+				return done();
+			});
+		});
+	});
+
+	context("sinon always err", function () {
+		before(function () {
+			console.log("sinon always err before");
+			mySinon.stopStub();
+			mySinon.errStub();
+		});
+		after(function () {
+			console.log("sinon always err after");
+			mySinon.stopStub();
+		});
+		it("errStub", function (done) {
+			myFunc.test(0, function (err, val) {
+				console.log(arguments);
+				err.should.equal("errStub");
+				return done();
+			});
+		});
+	});
 });
 
 
