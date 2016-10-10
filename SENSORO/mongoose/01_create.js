cac97c8e576013f1b22a354a366139bfa09f4f2e
wangyanqiang <wangyanqiang@sensoro.com>
2016-10-10 11:02:46 +0800

    add mongoose test

---
+++ SENSORO/mongoose/01_create.js
@@ -0,0 +1,60 @@
+/**
+ * Created by wyq on 16/10/10.
+ */
+"use strict";
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost/test");
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+let t1Schema = new Schema({
+	name: {type: String},
+	age: {type: Number},
+	addr: {type: String}
+});
+
+let t1Model = mongoose.model("t1", t1Schema);
+
+let t2Schema = new Schema({
+	job: {type: String},
+	love: {type: String},
+	t1Id: {type: mongoose.Schema.Types.ObjectId, ref: "t1"}
+});
+
+let t2Model = mongoose.model("t2", t2Schema);
+
+function test1() {
+	let doc = {
+		name: "AAAA",
+		age: 20,
+		addr: "beijing"
+	};
+	t1Model.create(doc, function (err, response) {
+		console.log(response);
+		let doc = {
+			job: "chengxuyuan",
+			love: "changge",
+			t1Id: response._id
+		};
+		t2Model.create(doc, function (err, response) {
+			console.log(response);
+		});
+	});
+}
+
+function test2() {
+	t2Model.findOne({_id: "57fb035353121ec02ce78a09"}).populate("t1Id").exec(function (err, response) {
+		console.log(response);
+	});
+}
+
+test2();
+
+
+
+
+
+
+
+
+
