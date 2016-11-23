531a6c07894432ad73cd6209c66f62649ed9d88f
wangyanqiang <wangyanqiang@sensoro.com>
2016-11-23 15:51:45 +0800

    1. openresty 学习

--- ztestE.js
+++ ztestE.js
@@ -1,10 +1,12 @@
 // curl -H "x-session-id: YKljIL29Ug-bTmyL3bvSr6VkHjYsRhY5" "http://qing.mocha.server.sensoro.com/pay/wxpay/order/refund" -d "refund_fee=1&reason=abcdefg&out_trade_no=2016103117573969669935270212"
 
-function test(flag) {
-	if (flag) {
-		return setTimeout(console.log, 5000, "asdfasdf");
-	}
-	return console.log("AAAAAAA");
+var redis = require("redis");
+var rc = redis.createClient(6379, "127.0.0.1");
+
+function test(cmd, args1, args2, arg3) {
+	rc[cmd](args1, args2);
 }
 
-test(true);
+test("keys", "*", function (err, response) {
+	console.log(arguments);
+});
