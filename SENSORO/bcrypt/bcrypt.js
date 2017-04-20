1aa98cbe396bcc8cf641f501800d2395bee68882
wangyanqiang <wangyanqiang@sensoro.com>
2017-04-20 17:55:43 +0800

    update

--- SENSORO/bcrypt/bcrypt.js
+++ SENSORO/bcrypt/bcrypt.js
@@ -1,4 +1,21 @@
 /**
  * Created by wyq on 17/3/29.
  */
-var bcrypt = require('bcrypt');
+"use strict";
+const bcrypt = require('bcrypt');
+let pwd = '$2a$04$fFTEcFPDz992G./DlysvAuMf8XCbhlkm7BQab/OXSleZaUflkLpBW';
+
+function t1() {
+	bcrypt.hash("123456", 1, function (err, hash) {
+		console.log(arguments);//注意: 没事生成的密码都会变化
+	});
+}
+
+function t2() {
+	bcrypt.compare("123456", pwd, function (err, isMatch) {
+		console.log(arguments);
+	});
+}
+
+
+
--- ztest1.js
+++ ztest1.js
@@ -1,9 +1,42 @@
-"use strict";
-
-var nums = [];
-for (let i = 0; i < 10; ++i) {
-	nums[i] = function (j) {
-		return i + j;
+var data = [
+	{
+		"_id": "58eafb50ecdead4d90756373",
+		"updatedTime": 1492663272536,
+		"email": "business1@sensoro.com",
+		"nickname": "business1",
+		"appId": "appId000003",
+		"appKey": "qQRgOxIwR4ODan6J4hKDpaBfBPKTh8eQdAF6ts6xTAP",
+		"appSecret": "MKLFSYfBgZJgdCNsN3xGdmKZBi6bRXi0",
+		"addBy": "58eafb50ecdead4d90756371",
+		"grants": {"user": ["update"], "device": ["get", "delete", "update", "list", "modifyCycle", "fence"]},
+		"baseInfo": {
+			"mobile": "18600945636",
+			"headUrl": "http://7u2jeb.com1.z0.glb.clouddn.com/d6afe7f957e5f280fbb9819ebab5e583.png"
+		},
+		"createdTime": 1491882153110,
+		"isDeleted": false,
+		"config": {"businessCount": 0, "businessLimit": 0},
+		"roles": "business",
+		"id": "58eafb50ecdead4d90756373"
+	},
+	{
+		"_id": "58eafb50ecdead4d90756374",
+		"updatedTime": 1492663272536,
+		"email": "business2@sensoro.com",
+		"nickname": "business2",
+		"appId": "appId000004",
+		"appKey": "qQRgOxIwR4ODan6J4hKDpaBfBPKTh8eQdAF6ts6xTAP",
+		"appSecret": "MKLFSYfBgZJgdCNsN3xGdmKZBi6bRXi0",
+		"addBy": "58eafb50ecdead4d90756371",
+		"grants": {"user": ["update"], "device": ["get", "delete", "update", "list", "modifyCycle", "fence"]},
+		"baseInfo": {
+			"mobile": "18600945636",
+			"headUrl": "http://7u2jeb.com1.z0.glb.clouddn.com/d6afe7f957e5f280fbb9819ebab5e583.png"
+		},
+		"createdTime": 1491882153110,
+		"isDeleted": false,
+		"config": {"businessCount": 0, "businessLimit": 0},
+		"roles": "business",
+		"id": "58eafb50ecdead4d90756374"
 	}
-}
-console.log(nums[0](2));
+];
