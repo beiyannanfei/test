7cd5d458d94b6538fa9c5ba9958afb3b487e4fd6
wangyanqiang <wangyanqiang@sensoro.com>
2017-04-26 17:04:41 +0800

    update

---
+++ SENSORO/msgCrypt.js
@@ -0,0 +1,28 @@
+/**
+ * Created by wyq on 17/4/26.
+ * 微信公共平台加解密库
+ */
+"use strict";
+const MsgCrypt = require("wechat-crypto");
+const _ = require("lodash");
+
+let config = {
+	appSecret: "abc123",
+	appKey: "4z04NBTlokkUjNZHK9WCfPPquYzkChtiRYHU4BHkHpf",    //必须为43位长度
+	appId: "cde789"
+};
+let msg = "wangyanqiang";
+let crypter = new MsgCrypt(config.appSecret, config.appKey, config.appId);
+let encrypted = crypter.encrypt(msg);   //每次加密后的值不同
+console.log("encrypted: %j", encrypted);
+let decrypted = crypter.decrypt(encrypted);
+console.log(decrypted);   //{ message: 'wangyanqiang', id: 'cde789' }
+
+
+
+
+
+
+
+
+
--- ztest1.js
+++ ztest1.js
@@ -1,25 +1,4 @@
-var text = {
-	"appId": "n7A3uIsqL58w",
-	"sn": "11440117C696504A",
-	"deviceType": "pm",
-	"type": "text",
-	"msgId": "58fe210213b51044cf8a8102",
-	"data": {"interval": 60, "battery": -1, "pm10": 13, "pm2_5": 12},
-	"createdTime": 1493049602528
-};
+let arr = ["a@a.com", false, false, "b@b.com"];
+arr = arr.filter(item => !!item);
+console.log(arr);
 
-var event = {
-	"appId": "n7A3uIsqL58w",
-	"sn": "11311017C642A4BD",
-	"type": "event",
-	"msgId": "58feb8b713b51044cf8ae693",
-	"taskId": 10583,
-	"data": {"resultCode": 1, "resultData": {}},
-	"createdTime": 1493088439443
-};
-var _ = require("underscore");
-
-var data = {a: 0};
-var upDoc = {};
-_.isUndefined(data.a) || (upDoc.sensorData.battery = data.a);
-console.log(upDoc);
