6547bd6a4139580219ec08ff3b51233873bdaa4c
wangyanqiang <wangyanqiang@sensoro.com>
2017-05-04 19:04:40 +0800

    update

--- ztest1.js
+++ ztest1.js
@@ -1,3 +1,16 @@
-var _ = require("underscore");
+var uuid = require("uuid");
 
-_.uniq()
+var GetUuid = function () {
+	var buffer = new Array(32);
+	uuid.v4(null, buffer, 0);
+	var string = uuid.unparse(buffer);
+	string = string.replace(/-/g, "");
+	return string;
+};
+
+let id = GetUuid();
+console.log(id);
+let id2 = id.substr(0, 2);
+console.log(id2);
+let id3 = parseInt(id2, 16);
+console.log(id3);
