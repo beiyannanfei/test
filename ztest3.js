b5dcebde7a32e4cadf976c5f5ca311c098c13bf6
wangyanqiang <wangyanqiang@sensoro.com>
2017-05-09 15:17:13 +0800

    update

--- ztest3.js
+++ ztest3.js
@@ -1,5 +1,23 @@
-var rc = require("redis").createClient();
-
-rc.hkeys("test", function (err, response) {
-	console.log(arguments);
-});
+var a = {
+	fenceName: "",
+	fenceIcon: "",
+	ruler: {
+		cycle: "",
+		startTime: "",
+		endTime: "",
+		enterIsEnable: "",
+		leaveIsEnable: ""
+	},
+	srcData: {
+		type: "",
+		lat: {
+			min:"",
+			max:""
+		},
+		lng: {
+			min:"",
+			max:""
+		},
+		radius: ""
+	}
+};
