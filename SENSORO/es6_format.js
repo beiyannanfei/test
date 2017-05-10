9d63c32ba6f26170bfaadfe7e64f160189b2f820
wangyanqiang <wangyanqiang@sensoro.com>
2017-05-10 10:57:55 +0800

    update

---
+++ SENSORO/es6_format.js
@@ -0,0 +1,14 @@
+/**
+ * Created by wyq on 17/5/9.
+ */
+let id = 123;
+let strTemp = `data is ${id}`;
+console.log(strTemp);
+
+let obj = {
+	a: {
+		b: 456
+	}
+};
+let str1 = `data is ${obj.a.b}`;
+console.log(str1);
