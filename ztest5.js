b7cdd3db169be35214ad0d1b2915d2809ee66601
wangyanqiang <wangyanqiang@sensoro.com>
2017-05-10 19:09:18 +0800

    update

--- ztest5.js
+++ ztest5.js
@@ -1,29 +1,25 @@
-var order = {
-	"dateTime": "2016-06-16T09:26:59.511Z",
-	"dateTimeStr": "2016-06-16 17:26:59",
-	"orderId": "1466069231109",
-	"timeStamp": "1466069231",
-	"userId": "wyq_test",
-	"mobile": "4167c3266b67fc449172249550a55876",
-	"productID": "测试2016",
-	"productName": "wyq测试",
-	"repayTime": "6个月",
-	"incomeRate": "11.00",
-	"money": 60001,
-	"loantypeName": "到期后一次性还息还本",
-	"feedback": "orEt2t9TyFNWO3XX5E-YTj61Hd0U|wx44490bbc768ce355|oxWE2s_3y2RcuEb1r6XSp2u9rZnQ|3",
-	"from": "huaren",
-	"needFx": 5000,
-	"redInfo": {
-		"redInfo.SrcCv": 9999979999,
-		"redInfo.spend": 5000,
-		"redInfo.firstSpend": 5000,
-		"redInfo.cV": 9999974999,
-		"redInfo.errState": -1
-	},
-	"_id": "576270e4740fdfb15fdc9ad3",
-	"yyyapp_id": "wx44490bbc768ce355",
-	"yyy_openid": "oxWE2s_3y2RcuEb1r6XSp2u9rZnQ",
-	"open_id": "orEt2t9TyFNWO3XX5E-YTj61Hd0U",
-	"product_name": "wyq测试"
-}
+var co = require("co");
+
+var asyncOperation = function () {
+	return function (callback) {
+		setTimeout(function () {
+			callback(null, 'this was loaded asynchronously and it took 2 seconds to complete')
+		}, 100);
+	}
+};
+
+let f1 = asyncOperation();
+f1(function (err, response) {
+	console.log(arguments);
+});
+/*
+ co(function *() {
+ let result = yield asyncOperation();
+ console.log(result);
+ return result;
+ }).then(val=> {
+ console.log("val: %j", val);
+ }).catch(err=> {
+ console.log("err: %j", err);
+ });
+ */
