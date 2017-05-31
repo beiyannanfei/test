fc618b63f66a9210503da2ac5bece534fe890652
wangyanqiang <wangyanqiang@sensoro.com>
2017-05-31 16:33:21 +0800

    update

--- ztestF.js
+++ ztestF.js
@@ -1,45 +1,8 @@
-{
-	"mongodb": {
-	"host": "localhost",
-		"database": "smart-city-api"
-},
+let customerData = "6301";
 
-	"redis": {
-	"host": "localhost",
-		"port": 6379,
-		"databaseIndex": 1
-},
-	"redis_config": {
-	"expireTime_time": 5
-},
-
-	"bcrypt": {
-	"rounds": 1
-},
-
-	"sessionSecret": "test",
-
-	"yunpian": {
-	// 短信发送host
-	"sms_host":"sms.yunpian.com",
-		// 指定模板发送接口https地址
-		"send_tpl_sms_uri":"/v1/sms/tpl_send.json",
-		//用户key
-		"api_key": "26d4084c4cc0902925459f20ead5604a",
-		"system": {
-		"alarm_tpl_id": "1699228", //报警模板id
-			"ok_tpl_id": "1699230" // 恢复正常模板id
-	}
-},
-	"logger": {
-	"logDir": "/logs",
-		"level": "debug",
-		"output": true
-},
-	"qiniu": {
-	"bucketname": "sensoro-wechat",
-		"ACCESS_KEY": "ds-uUH_gLTmmdWvXIcIIUxCIgM6LZ6MKJlBbxoJs",
-		"SECRET_KEY": "AGLX0HQDa9HCHzbACtWrNUznUV5GepKSTlT8t6rU",
-		"domain": "http://7u2jeb.com1.z0.glb.clouddn.com/"
-}
-}
+let BATTERY = customerData.slice(0, 2);
+console.log(BATTERY);
+BATTERY = parseInt(BATTERY, 16);
+console.log(BATTERY);
+let jinggai = customerData.slice(2);
+console.log(jinggai);
