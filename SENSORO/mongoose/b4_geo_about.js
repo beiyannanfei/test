6e5e725207f687fbc1e3ba93ac87e5746e390769
wangyanqiang <wangyanqiang@sensoro.com>
2017-06-01 18:05:50 +0800

    update

--- SENSORO/mongoose/b4_geo_about.js
+++ SENSORO/mongoose/b4_geo_about.js
@@ -1,13 +1,14 @@
 /**
  * Created by wyq on 17/6/1.
- * 地理围栏相关测试,图见同名文件图片
+ * 地理围栏相关测试,图见同名文件图片(参考文档:http://www.infoq.com/cn/articles/depth-study-of-Symfony2/)
  */
 "use strict";
 const mongoose = require('mongoose');
-mongoose.Promise = require('bluebird');
+const Bluebird = require("bluebird");
+mongoose.Promise = Bluebird;
 mongoose.connect("mongodb://localhost/test");
 const Schema = mongoose.Schema;
-const Bluebird = require("bluebird");
+
 const _ = require("underscore");
 
 let t1Schema = new Schema({
@@ -93,7 +94,7 @@ function save2db() {
 }
 
 /**
- * 查询当前坐标center附近的目标，由近到远排列
+ * 查询当前坐标center附近的目标，由近到远排列(距离单位为弧度),球面距离
  */
 function t1() {
 	t1Model.find({lonlat: {$nearSphere: center}}).limit(3).exec().then(val => {
@@ -104,7 +105,7 @@ function t1() {
 }
 
 /**
- * 指定最大距离,这里用near，默认以度为单位，公里数除以111(注意: $near不能使用2dsphere类型索引)
+ * 指定最大距离,这里用near，默认以度为单位，公里数除以111(注意: $near不能使用2dsphere类型索引),平面距离
  */
 function t2() {
 	t1Model.find({lonlat2: {$near: center, $maxDistance: 2}}).then(val => {
@@ -115,7 +116,7 @@ function t2() {
 }
 
 /**
- * $box 矩形区域内搜索(查询结果为按照距离进行排序)
+ * $box 矩形区域内搜索(查询结果为乱序的)
  */
 function t3() {
 	let maxLonlat = doc.find(item => item.name === "b1");
@@ -134,7 +135,73 @@ function t3() {
 		console.log("t3 err: %j", err.message || err);
 	});
 }
-t3();
+
+/**
+ * 2d索引能同时支持$center和$centerSphere，2dsphere索引支持$centerSphere。
+ * 关于距离单位，$center默认是度，$centerSphere默认距离是弧度。
+ * 圆形区域 查询以某坐标为圆心，指定半径的圆内的数据
+ */
+function t4() {
+	let radius = 280 / 6371;  //通过距离计算弧度半径
+	t1Model.find({
+		lonlat: {
+			$geoWithin: {
+				$centerSphere: [center, radius]
+			}
+		}
+	}).then(val => {
+		console.log(_.pluck(val, "name"));
+	}).catch(err => {
+		console.log("t4 err: %j", err.message || err);
+	});
+}
+
+/**
+ * 多边形
+ */
+function t5() {
+	let point1 = center;
+	let point2 = doc.find(item => item.name === "c1").lonlat;
+	let point3 = doc.find(item => item.name === "a2").lonlat;
+	t1Model.find({
+		lonlat: {
+			$geoWithin: {
+				$polygon: [
+					point1,
+					point2,
+					point3
+				]
+			}
+		}
+	}).then(val => {
+		console.log(_.pluck(val, "name"));
+	}).catch(err => {
+		console.log("t5 err: %j", err.message || err);
+	});
+}
+
+/**
+ * GeoJson $maxDistance距离单位默认为米
+ */
+function t6() {
+	t1Model.find({
+		lonlat: {
+			$nearSphere: {
+				$geometry: {
+					type: "Point",
+					coordinates: center
+				},
+				$minDistance: 140604,        //distance in meters
+				$maxDistance: 350000
+			}
+		}
+	}).then(val => {
+		console.log(_.pluck(val, "name"));
+	}).catch(err => {
+		console.log("t6 err: %j", err.message || err);
+	});
+}
+
 
 
 
