0529805667515fb60c18e13c859a57477e07e42d
wangyanqiang <wangyanqiang@sensoro.com>
2017-06-01 13:36:04 +0800

    update

--- ztestE.js
+++ ztestE.js
@@ -1,27 +1,5 @@
-var a = {
-	"updatedTime": 1495785273801,
-	"interval": 60,
-	"appId": "SJRgSBj4cYds",
-	"sn": "10C01017C69E7BD8",
-	"lastUpdatedTime": "2017-05-26T07:53:33.778Z",
-	"owners": "58df0d85ba2bee6f4b7aaa97",
-	"name": "中山大街118号王多多-一楼厨房-中山大街118号",
-	"createTime": 1484718776526,
-	"alarmsRecords": [],
-	"alarmStatus": 1,
-	"status": 3,
-	"alarms": {
-		"createTime": "2017-01-18T05:52:56.526Z",
-		"notification": {"content": "13813063829", "types": "phone"},
-		"rules": [{"_id": "59255e528ddc046007b2c56f", "conditionType": "gt", "thresholds": 5000, "sensorTypes": "ch4"}]
-	},
-	"lonlat": [118.867372, 31.320729],
-	"tags": ["高淳老街"],
-	"sensorData": {"customer": "35bd3786354863503c", "battery": 99, "interval": 60, "ch4": 0},
-	"deviceType": "module",
-	"sensorTypes": ["ch4"],
-	"_level": 3,
-	"level_display": "未激活/0ppm",
-	"alarms_display": "甲烷大于 5000ppm",
-	"_updatedTime": "2017-05-26 15:54:33"
-};
+let sensorTypes = ["a", "b", "c", "d"];
+let levels = {};
+levels[sensorTypes] = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0};
+console.log(levels);
+
