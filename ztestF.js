487796997c8f0ad8293cb04d892f52a6652d6964
wangyanqiang <wangyanqiang@sensoro.com>
2017-06-08 10:00:31 +0800

    update

--- ztestF.js
+++ ztestF.js
@@ -1,16 +1,20 @@
-var debug = require('debug')('http')
-	, http = require('http')
-	, name = 'My App';
+var a = [
+	{
+		a: 10,
+		b: 20
+	},
+	{
+		$group: {
+			_id: {
+				year: {$year: '$updatedTime'},
+				month: {$month: '$updatedTime'},
+				day: {$dayOfMonth: "$updatedTime"},
+				hour: {$hour: "$updatedTime"}
+			},
+			count: {$sum: 1}
+		}
+	}
+];
 
-// fake app
-
-debug('booting %s', name);
-
-http.createServer(function(req, res){
-	debug(req.method + ' ' + req.url);
-	res.end('hello\n');
-}).listen(3000, function(){
-	debug('listening');
-});
-
-require("./ztestE");
+delete a[1].$group._id.hour;
+console.log("%j", a);
