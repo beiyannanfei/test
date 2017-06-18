0becb281145dcd0041e588f21a729cabcfb1ba4e
wangyanqiang <wangyanqiang@sensoro.com>
2017-06-18 15:07:26 +0800

    update

--- ztestA.js
+++ ztestA.js
@@ -1,19 +1,8 @@
-"gps" : {"altitude" : 327.2,		"longitude" : 11628.5754,		"latitude" : 3959.6232}
-"gps" : {	"altitude" : 453.4,		"longitude" : 11628.6581,		"latitude" : 3959.5616}
-"gps" : {	"altitude" : 70.5,	"longitude" : 11628.4587,	"latitude" : 3959.718}
-"gps" : {	"altitude" : 238.9,		"longitude" : 11628.5827,		"latitude" : 3959.6422}
-"gps" : {	"altitude" : 240.1,		"longitude" : 11628.5987,		"latitude" : 3959.6223}
-"gps" : {	"altitude" : 255.6,		"longitude" : 11628.5589,		"latitude" : 3959.6446}
-"gps" : {	"altitude" : 147.1,		"longitude" : 11628.5239,		"latitude" : 3959.6675}
-"gps" : {	"altitude" : 151.6,		"longitude" : 11628.5116,		"latitude" : 3959.6685}
 
-
-
-
-
-
-
-
-
-
-
+for (var i = 1; i <= 99; ++i) {
+	if (i < 10) {
+		console.log(i+" ");
+		continue;
+	}
+	console.log(i);
+}
