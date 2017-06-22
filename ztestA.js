9992641172040f407bc6cce9f8cae5e580368a6d
wangyanqiang <wangyanqiang@sensoro.com>
2017-06-22 10:13:06 +0800

    update

--- ztestA.js
+++ ztestA.js
@@ -1,2 +1,217 @@
-var a = "asdf";
-console.log("[*] err is", a);
+var a = {
+	"errcode": 0,
+	"count": 86,
+	"data": [
+		{
+			"_id": "5948ca4aff4e7907f958bb1c",
+			"typeDesc": "notification of entering the fence",
+			"users": "5909a7e08fc0634c63459139",
+			"sn": "01581117C6E54A93",
+			"deviceName": "NJ",
+			"type": 0,
+			"content": "Your device NJ(01581117C6E54A93) enters the fence range you have set on 2017-06-20 15:10:02.",
+			"createdTime": 1497942602452,
+			"emails": ["54wvqso@163.com"],
+			"lonlat": [116.4816799315, 39.9959162185],
+			"indexTags": ["NJ", "01581117C6E54A93", "54wvqso@163.com", "notification of entering the fence"],
+			"id": "5948ca4aff4e7907f958bb1c",
+			"deviceInfo": {
+				"sn": "01581117C6E54A93",
+				"updatedTime": 1498037775557,
+				"deviceName": "NJ",
+				"sensorData": {"battery": 83},
+				"tags": [],
+				"_alarmSwitch": false
+			}
+		},
+		{
+			"_id": "5948c9bbff4e7907f958bb15",
+			"typeDesc": "notification of leaving the fence",
+			"users": "5909a7e08fc0634c63459139",
+			"sn": "01581117C6E54A93",
+			"deviceName": "NJ",
+			"type": 1,
+			"content": "Your device NJ(01581117C6E54A93) leaves the fence range you have set on 2017-06-20 15:07:39.",
+			"createdTime": 1497942459354,
+			"emails": ["54wvqso@163.com"],
+			"lonlat": [116.48212, 39.9943224343],
+			"indexTags": ["NJ", "01581117C6E54A93", "54wvqso@163.com", "notification of leaving the fence"],
+			"id": "5948c9bbff4e7907f958bb15",
+			"deviceInfo": {
+				"sn": "01581117C6E54A93",
+				"updatedTime": 1498037775557,
+				"deviceName": "NJ",
+				"sensorData": {"battery": 83},
+				"tags": [],
+				"_alarmSwitch": false
+			}
+		}, {
+			"_id": "5948c77eff4e7907f958bafe",
+			"typeDesc": "notification of entering the fence",
+			"users": "5909a7e08fc0634c63459139",
+			"sn": "01581117C6E54A93",
+			"deviceName": "NJ",
+			"type": 0,
+			"content": "Your device NJ(01581117C6E54A93) enters the fence range you have set on 2017-06-20 14:58:06.",
+			"createdTime": 1497941886224,
+			"emails": ["54wvqso@163.com"],
+			"lonlat": [116.4822843908, 39.99545036],
+			"indexTags": ["NJ", "01581117C6E54A93", "54wvqso@163.com", "notification of entering the fence"],
+			"id": "5948c77eff4e7907f958bafe",
+			"deviceInfo": {
+				"sn": "01581117C6E54A93",
+				"updatedTime": 1498037775557,
+				"deviceName": "NJ",
+				"sensorData": {"battery": 83},
+				"tags": [],
+				"_alarmSwitch": false
+			}
+		}, {
+			"_id": "5948c737ff4e7907f958bafa",
+			"typeDesc": "notification of leaving the fence",
+			"users": "5909a7e08fc0634c63459139",
+			"sn": "01581117C6E54A93",
+			"deviceName": "NJ",
+			"type": 1,
+			"content": "Your device NJ(01581117C6E54A93) leaves the fence range you have set on 2017-06-20 14:56:55.",
+			"createdTime": 1497941815668,
+			"emails": ["54wvqso@163.com"],
+			"lonlat": [116.4840543237, 39.9949719035],
+			"indexTags": ["NJ", "01581117C6E54A93", "54wvqso@163.com", "notification of leaving the fence"],
+			"id": "5948c737ff4e7907f958bafa",
+			"deviceInfo": {
+				"sn": "01581117C6E54A93",
+				"updatedTime": 1498037775557,
+				"deviceName": "NJ",
+				"sensorData": {"battery": 83},
+				"tags": [],
+				"_alarmSwitch": false
+			}
+		}, {
+			"_id": "5948c6abff4e7907f958baf3",
+			"typeDesc": "notification of entering the fence",
+			"users": "5909a7e08fc0634c63459139",
+			"sn": "01581117C6E54A93",
+			"deviceName": "NJ",
+			"type": 0,
+			"content": "Your device NJ(01581117C6E54A93) enters the fence range you have set on 2017-06-20 14:54:35.",
+			"createdTime": 1497941675514,
+			"emails": ["54wvqso@163.com"],
+			"lonlat": [116.4818462766, 39.9954055707],
+			"indexTags": ["NJ", "01581117C6E54A93", "54wvqso@163.com", "notification of entering the fence"],
+			"id": "5948c6abff4e7907f958baf3",
+			"deviceInfo": {
+				"sn": "01581117C6E54A93",
+				"updatedTime": 1498037775557,
+				"deviceName": "NJ",
+				"sensorData": {"battery": 83},
+				"tags": [],
+				"_alarmSwitch": false
+			}
+		}, {
+			"_id": "5948c665ff4e7907f958baef",
+			"typeDesc": "notification of leaving the fence",
+			"users": "5909a7e08fc0634c63459139",
+			"sn": "01581117C6E54A93",
+			"deviceName": "NJ",
+			"type": 1,
+			"content": "Your device NJ(01581117C6E54A93) leaves the fence range you have set on 2017-06-20 14:53:25.",
+			"createdTime": 1497941605784,
+			"emails": ["54wvqso@163.com"],
+			"lonlat": [116.4833354818, 39.9942432178],
+			"indexTags": ["NJ", "01581117C6E54A93", "54wvqso@163.com", "notification of leaving the fence"],
+			"id": "5948c665ff4e7907f958baef",
+			"deviceInfo": {
+				"sn": "01581117C6E54A93",
+				"updatedTime": 1498037775557,
+				"deviceName": "NJ",
+				"sensorData": {"battery": 83},
+				"tags": [],
+				"_alarmSwitch": false
+			}
+		}, {
+			"_id": "5948a9c5ff4e7907f958bad9",
+			"typeDesc": "notification of entering the fence",
+			"users": "5909a7e08fc0634c63459139",
+			"sn": "01581117C6E54A93",
+			"deviceName": "NJ",
+			"type": 0,
+			"content": "Your device NJ(01581117C6E54A93) enters the fence range you have set on 2017-06-20 12:51:17.",
+			"createdTime": 1497934277378,
+			"emails": ["54wvqso@163.com"],
+			"lonlat": [116.4819933233, 39.9959528541],
+			"indexTags": ["NJ", "01581117C6E54A93", "54wvqso@163.com", "notification of entering the fence"],
+			"id": "5948a9c5ff4e7907f958bad9",
+			"deviceInfo": {
+				"sn": "01581117C6E54A93",
+				"updatedTime": 1498037775557,
+				"deviceName": "NJ",
+				"sensorData": {"battery": 83},
+				"tags": [],
+				"_alarmSwitch": false
+			}
+		}, {
+			"_id": "5948a970ff4e7907f958bad5",
+			"typeDesc": "notification of leaving the fence",
+			"users": "5909a7e08fc0634c63459139",
+			"sn": "01581117C6E54A93",
+			"deviceName": "NJ",
+			"type": 1,
+			"content": "Your device NJ(01581117C6E54A93) leaves the fence range you have set on 2017-06-20 12:49:52.",
+			"createdTime": 1497934192832,
+			"emails": ["54wvqso@163.com"],
+			"lonlat": [116.476766005, 39.996865234],
+			"indexTags": ["NJ", "01581117C6E54A93", "54wvqso@163.com", "notification of leaving the fence"],
+			"id": "5948a970ff4e7907f958bad5",
+			"deviceInfo": {
+				"sn": "01581117C6E54A93",
+				"updatedTime": 1498037775557,
+				"deviceName": "NJ",
+				"sensorData": {"battery": 83},
+				"tags": [],
+				"_alarmSwitch": false
+			}
+		}, {
+			"_id": "5948a90eff4e7907f958bad1",
+			"typeDesc": "notification of entering the fence",
+			"users": "5909a7e08fc0634c63459139",
+			"sn": "01581117C6E54A93",
+			"deviceName": "NJ",
+			"type": 0,
+			"content": "Your device NJ(01581117C6E54A93) enters the fence range you have set on 2017-06-20 12:48:14.",
+			"createdTime": 1497934094291,
+			"emails": ["54wvqso@163.com"],
+			"lonlat": [116.4806465752, 39.9958463903],
+			"indexTags": ["NJ", "01581117C6E54A93", "54wvqso@163.com", "notification of entering the fence"],
+			"id": "5948a90eff4e7907f958bad1",
+			"deviceInfo": {
+				"sn": "01581117C6E54A93",
+				"updatedTime": 1498037775557,
+				"deviceName": "NJ",
+				"sensorData": {"battery": 83},
+				"tags": [],
+				"_alarmSwitch": false
+			}
+		}, {
+			"_id": "5948a8c1ff4e7907f958bacd",
+			"typeDesc": "notification of leaving the fence",
+			"users": "5909a7e08fc0634c63459139",
+			"sn": "01581117C6E54A93",
+			"deviceName": "NJ",
+			"type": 1,
+			"content": "Your device NJ(01581117C6E54A93) leaves the fence range you have set on 2017-06-20 12:46:57.",
+			"createdTime": 1497934017585,
+			"emails": ["54wvqso@163.com"],
+			"lonlat": [116.483854336, 39.9959670065],
+			"indexTags": ["NJ", "01581117C6E54A93", "54wvqso@163.com", "notification of leaving the fence"],
+			"id": "5948a8c1ff4e7907f958bacd",
+			"deviceInfo": {
+				"sn": "01581117C6E54A93",
+				"updatedTime": 1498037775557,
+				"deviceName": "NJ",
+				"sensorData": {"battery": 83},
+				"tags": [],
+				"_alarmSwitch": false
+			}
+		}]
+}
