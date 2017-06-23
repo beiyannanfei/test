dae3bf19c5e5899de4440b57ef7eb46c55bf22d8
wangyanqiang <wangyanqiang@sensoro.com>
2017-06-23 16:49:14 +0800

    update

--- ztestA.js
+++ ztestA.js
@@ -1,27 +1,25 @@
-var a = {
-	"errcode": 0,
-	"data": [
-		{
-			"_id": "58f47db2ecdead4d90776e85",
-			"sn": "a000000002",
-			"devices": {
-				"_id": "58f4743aecdead5d00776af9",
-				"sn": "a000000002",
-				"deviceName": "设备名称2",
-				"updatedTime": 1492415544066,
-				"sensorData": {"battery": 85},
-				"tags": ["tag3", "tag4"],
-				"_alarmSwitch": false,
-				"id": "58f4743aecdead5d00776af9"
-			},
-			"users": "58eafb50ecdead4d90756371",
-			"createTime": 1492416877584,
-			"lonlat": [-122.214, 37.772],
-			"status": 2,
-			"statusDesc": "Move",
-			"createdTimeStr": "2017-04-17 16:14:37",
-			"id": "58f47db2ecdead4d90776e85"
-		}
-	],
-	"count": 1
+const moment = require("moment");
+let item = {
+	"_id": "594b78d82ebb822d6e7034fb",
+	"typeDesc": "notification of entering the fence",
+	"users": "5909a7e08fc0634c63459139",
+	"sn": "01581117C6E54A93",
+	"deviceName": "NJ",
+	"type": 0,
+	"content": "Your device NJ(01581117C6E54A93) enters the fence range you have set on 2017-06-22 15:59:20.",
+	"createdTime": 1498118360760,
+	"emails": ["54wvqso@163.com"],
+	"lonlat": [116.4813007638, 39.9958292599],
+	"indexTags": ["NJ", "01581117C6E54A93", "54wvqso@163.com", "notification of entering the fence"],
+	"id": "594b78d82ebb822d6e7034fb",
+	"deviceInfo": {
+		"sn": "01581117C6E54A93",
+		"updatedTime": 1498203973490,
+		"deviceName": "NJ",
+		"sensorData": {"battery": 82},
+		"tags": [],
+		"_alarmSwitch": false
+	}
 };
+item.deviceInfo && item.deviceInfo.updatedTime && (item.devices.updatedTime = moment(+item.devices.updatedTime).format('YYYY-MM-DD HH:mm:ss'));
+console.log(item);
