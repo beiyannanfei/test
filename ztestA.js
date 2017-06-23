6db7dadc7ec35b6d5408b75c735ee84c52febd14
wangyanqiang <wangyanqiang@sensoro.com>
2017-06-23 14:44:57 +0800

    update

--- ztestA.js
+++ ztestA.js
@@ -1,23 +1,27 @@
-let condition = [
-	{
-		$group: {
-			_id: {
-				year: {
-					$year: "$updatedTime"
-				},
-				month: {
-					$month: "$updatedTime"
-				},
-				day: {
-					$dayOfMonth: "$updatedTime"
-				},
-				hour: {
-					$hour: "$updatedTime"
-				},
+var a = {
+	"errcode": 0,
+	"data": [
+		{
+			"_id": "58f47db2ecdead4d90776e85",
+			"sn": "a000000002",
+			"devices": {
+				"_id": "58f4743aecdead5d00776af9",
+				"sn": "a000000002",
+				"deviceName": "设备名称2",
+				"updatedTime": 1492415544066,
+				"sensorData": {"battery": 85},
+				"tags": ["tag3", "tag4"],
+				"_alarmSwitch": false,
+				"id": "58f4743aecdead5d00776af9"
 			},
-			count: {
-				$sum: 1
-			}
+			"users": "58eafb50ecdead4d90756371",
+			"createTime": 1492416877584,
+			"lonlat": [-122.214, 37.772],
+			"status": 2,
+			"statusDesc": "Move",
+			"createdTimeStr": "2017-04-17 16:14:37",
+			"id": "58f47db2ecdead4d90776e85"
 		}
-	}
-];
+	],
+	"count": 1
+};
