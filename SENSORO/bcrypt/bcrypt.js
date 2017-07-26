132d516868462b08f1fd0439b3cda62df2e60fc4
wangyanqiang <wangyanqiang@sensoro.com>
2017-07-26 18:56:11 +0800

    update

--- SENSORO/bcrypt/bcrypt.js
+++ SENSORO/bcrypt/bcrypt.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcrypt');
 let pwd = '$2a$04$fviiDpvl.ae7.whPgrI6YuiX7H5axqtdBQeT23T6EnYYGfUYiOGz.';
 
 function t1() {
-	bcrypt.hash("123456", 1, function (err, hash) {
+	bcrypt.hash("7H5axqtdBQeT2", 1, function (err, hash) {
 		console.log(arguments);//注意: 没事生成的密码都会变化
 	});
 }
@@ -17,7 +17,7 @@ function t2() {
 	});
 }
 
-t2();
+t1();
 
 
 
