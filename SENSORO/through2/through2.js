9f734ee23f9bb3094e2f87c231c778d102deb033
wangyanqiang <wangyanqiang@sensoro.com>
2017-08-02 14:42:35 +0800

    update

--- SENSORO/through2/through2.js
+++ SENSORO/through2/through2.js
@@ -6,25 +6,52 @@ const through2 = require("through2");
 const csv2 = require("csv2");
 const fs = require("fs");
 
-fs.createReadStream("./e0b7833431acbd4a529115c92bcf0b7a.csv")
-	.pipe(csv2(/*{separator: "\t"}*/))
-	.pipe(through2.obj(function (chunk, enc, cb) {
-		// console.log(chunk);
-		let dataArr;
-		try {
-			dataArr = chunk[0].split("\t");
-		} catch (e) {
-			console.log(e.message, chunk);
-		}
-		this.push(dataArr);
-		return cb();
-	}))
-	.on("data", data => {
-		console.log("====data: %j", data);
-	})
-	.on("error", err => {
-		console.log("=== err: %s", err.message || err);
-	})
-	.on("end", () => {
-		console.log("==== end");
-	});
+function test1() {
+	fs.createReadStream("./e0b7833431acbd4a529115c92bcf0b7a.csv")
+		.pipe(csv2(/*{separator: "\t"}*/))
+		.pipe(through2.obj(function (chunk, enc, cb) {
+			// console.log(chunk);
+			let dataArr;
+			try {
+				dataArr = chunk[0].split("\t");
+			} catch (e) {
+				console.log(e.message, chunk);
+			}
+			this.push(dataArr);
+			return cb();
+		}))
+		.on("data", data => {
+			console.log("====data: %j", data);
+		})
+		.on("error", err => {
+			console.log("=== err: %s", err.message || err);
+		})
+		.on("end", () => {
+			console.log("==== end");
+		});
+}
+
+function test2() {
+	fs.createReadStream("./5857c3f4cc07085cacda92ee-abcd1234.csv")
+		.pipe(csv2())
+		.pipe(through2.obj(function (chunk, enc, cb) {
+			let dataArr;
+			try {
+				dataArr = chunk[0].split("\t");
+			} catch (e) {
+				console.log(e.message, chunk);
+			}
+			this.push(dataArr);
+			return cb();
+		}))
+		.on("data", data => {
+			console.log("====data: %j", data);
+		})
+		.on("error", err => {
+			console.log("=== err: %s", err.message || err);
+		})
+		.on("end", () => {
+			console.log("==== end");
+		});
+}
+test2()
