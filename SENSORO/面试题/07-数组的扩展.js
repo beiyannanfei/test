1fe2425b948445920d5118447dbc453286c191cf
wangyanqiang <wangyanqiang@sensoro.com>
2017-08-10 11:34:38 +0800

    update

--- SENSORO/面试题/07-数组的扩展.js
+++ SENSORO/面试题/07-数组的扩展.js
@@ -36,7 +36,73 @@ function t3() {
 	let arr6 = Array.from({length: 2}, () => 'jack');
 	console.log("arr6 = %j", arr6); //arr6 = ["jack","jack"]
 }
-t3();
+
+function t4() {
+	let arr = [1, 5, 10, 15];
+	let a = arr.find((value, index, arr) => {
+		/*              值     下标   原数组
+		 1 0 [ 1, 5, 10, 15 ]
+		 5 1 [ 1, 5, 10, 15 ]
+		 10 2 [ 1, 5, 10, 15 ]
+		 15 3 [ 1, 5, 10, 15 ]
+		 */
+		console.log(value, index, arr);
+		return value > 10;
+	});
+	console.log("a = %j", a); //a = 15
+}
+
+function t5() {
+	let arr = [1, 5, 10, 15];
+	let aIn = arr.findIndex((value, index, arr) => {  //返回第一个符合条件的数组成员的位置，如果所有成员都不符合条件，则返回-1
+		return value > 10;
+	});
+	console.log("aIn = %j", aIn); //aIn = 3
+}
+
+function t6() {
+	let aIndex = [NaN].indexOf(NaN);      //indexOf不能发现NaN
+	console.log("aIndex = %j", aIndex); //aIndex = -1
+	let bIndex = [NaN].findIndex(val => { //findIndex方法可以借助Object.is方法做到识别NaN
+		return Object.is(NaN, val);
+	});
+	console.log("bIndex = %j", bIndex); //bIndex = 0
+}
+
+function t7() {
+	var arr1 = ["a", "b", "c"];
+	arr1.fill(7); //fill方法使用给定值，填充一个数组
+	console.log("arr1 = %j", arr1); //arr1 = [7,7,7]
+}
+
+function t8() {
+	let arr = ["a", "b", "c"];
+	for (let index of arr.keys()) { //keys()是对键名的遍历
+		console.log("index = %j", index);//index = 0		index = 1		index = 2
+	}
+	console.log("==================");
+	for (let value of arr) {
+		console.log("value = %j", value); //value = "a"		value = "b"		value = "c"
+	}
+	console.log("==================");
+	for (let [index, value] of arr.entries()) { //entries()是对键值对的遍历
+		console.log(`index = ${index}, value = ${value}`);
+		//index = 0, value = a		index = 1, value = b		index = 2, value = c
+	}
+}
+
+function t9() {
+	let isHas1 = [1, 2, 3].includes(2);
+	console.log(`isHas1 is ${isHas1}`); //isHas1 is true
+
+	let isHas2 = [1, 2, NaN, 3].includes(NaN);
+	console.log(`isHas2 is ${isHas2}`); //isHas2 is true
+
+	let isHas3 = [1, 2, 3, 4].includes(1, 1); //该方法的第二个参数表示搜索的起始位置，默认为0。如果第二个参数为负数，则表示倒数的位置
+	console.log(`isHas3 is ${isHas3}`); //isHas3 is false
+}
+
+
 
 
 
