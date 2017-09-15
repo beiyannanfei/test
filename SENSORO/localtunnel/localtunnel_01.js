fb1404dff00c20a0c086d4ad3a5d8ce14e190921
wangyanqiang <wangyanqiang@sensoro.com>
2017-09-15 18:40:57 +0800

    update

---
+++ SENSORO/localtunnel/localtunnel_01.js
@@ -0,0 +1,16 @@
+/**
+ * Created by wyq on 17/9/15.
+ */
+var localtunnel = require('localtunnel');
+
+var tunnel = localtunnel(3000, {subdomain: "wangyanqiang"}, function (err, tunnel) {
+	if (!!err) {
+		return console.log("err: %j", err.message || err);
+	}
+	console.log("url: %j", tunnel.url);       //这个url就是代理的本地3000端口
+	return console.log("tunnel: %j", tunnel);
+});
+
+tunnel.on('close', function () {
+	return console.log("close: %j", arguments);
+});
