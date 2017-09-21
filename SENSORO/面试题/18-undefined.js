53e7be3285f707b57cccf6b260f0b3231fb60e89
wangyanqiang <wangyanqiang@sensoro.com>
2017-09-21 14:52:46 +0800

    update

---
+++ SENSORO/面试题/18-undefined.js
@@ -0,0 +1,11 @@
+/**
+ * Created by wyq on 17/9/21.
+ * undefined和null的区别
+ */
+
+console.log(Number(undefined));   //NaN       undefined表示"缺少值"，就是此处应该有一个值，但是还没有定义。
+console.log(Number(null));        //0         null表示"没有对象"，即该处不应该有值。
+
+
+
+
