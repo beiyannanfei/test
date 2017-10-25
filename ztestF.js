99760effceb31a9e2574a295e83c9b732229b910
wangyanqiang <wangyanqiang@sensoro.com>
2017-10-25 15:31:34 +0800

    update

--- ztestF.js
+++ ztestF.js
@@ -1,4 +1,12 @@
-let obj = {};
-let a = true;
-a && (obj.a = 10) && (obj.b = 20);
-console.log(obj);
+const fs = require("fs");
+const path = require("path");
+app.get("/down", function (req, res) {
+	let filePath = path.join(__dirname, `../files`);
+	console.log("=======filePath: %j", filePath);
+	if (fs.existsSync(filePath)) {
+		console.log("文件存在")
+	}
+	res.download(filePath, "aaa.mp4", function (err) {
+		console.log("======= args: %j", arguments);
+	});
+});
