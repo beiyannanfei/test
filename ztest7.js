fbbef9f2179ab98067c765df9f2131251b4fe5b7
wangyanqiang <wangyanqiang@sensoro.com>
2017-11-06 14:46:39 +0800

    update

--- ztest7.js
+++ ztest7.js
@@ -1,2 +1,6 @@
-let condition = {口味: "清真", money: {$lte: 100}};
-区 虎 键 其 享 席 徐 硫 吴 销 全 赞 样 寸 希 劲 纳 构 血 细 愈 扩 学 本 纳
+let a = "{\"phoneList\":[{\"name\":\"15592060000\",\"number\":\"15592060000\"}],\"emailList\":[]}";
+console.log(JSON.parse(a));
+let phoneList = [{name: '15592060000', number: '15592060000'}];
+console.log(phoneList.toString());
+const _ = require("");
+console.log(_.pluck(phoneList, "number").toString());
