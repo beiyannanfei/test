16b9f486b122f8c6ee5731dcabde376fa4cb0bb6
wangyanqiang <wangyanqiang@sensoro.com>
2017-11-15 14:58:49 +0800

    update

--- zTest/ztest8.js
+++ zTest/ztest8.js
@@ -1,3 +1,4 @@
+/*
 monitor - _deviceDeploy 传感器部署(a d b)
 monitor - _heatmap 人员密度(a d b)
 
@@ -8,7 +9,7 @@ device - _detailBtn 查看详情按钮(a d b)
 indoorMap - _autoMonitor 自动监控(a d b)
 
 account - _modifyGrant 修改权限模板(a d b)
-
+*/
 
 
 
