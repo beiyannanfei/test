2b8e3ab2300ac94549dcabc4a611acb253241a8c
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-17 17:11:52 +0800

    图片裁切

---
+++ SENSORO/图片裁切/t.js
@@ -0,0 +1,39 @@
+/**
+ * Created by wyq on 18/1/17.
+ */
+const Jimp = require("jimp");
+const fs = require("fs");
+
+// const imageBuf = fs.readFileSync("./a.png");
+
+
+function resize() {
+	const newFileName = "./a_resize.png";
+	if (fs.existsSync(newFileName)) {    //如果文件已经存在则删除
+		fs.unlinkSync(newFileName);
+	}
+	Jimp.read("./a.png").then(lenna => {
+		lenna.resize(256, 256)
+			.write(newFileName);
+	}).catch(err => {
+		return console.log("resize err: %j", err.message || err);
+	});
+}
+
+function crop() {
+	const newFileName = "./a_crop.png";
+	if (fs.existsSync(newFileName)) {    //如果文件已经存在则删除
+		fs.unlinkSync(newFileName);
+	}
+	Jimp.read("./a.png").then(lenna => {
+		lenna.crop(10, 10, 100, 100)
+			.write(newFileName);
+	}).catch(err => {
+		return console.log("crop err: %j", err.message || err);
+	});
+}
+
+crop();
+
+
+
