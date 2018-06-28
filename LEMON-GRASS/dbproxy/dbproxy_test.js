e7c45217a8460d01c1076dc9d977494899dd83e8
wyq <18810776836@163.com>
2018-06-28 12:05:22 +0800

    update

--- LEMON-GRASS/dbproxy/dbproxy_test.js
+++ LEMON-GRASS/dbproxy/dbproxy_test.js
@@ -1,3 +1,47 @@
-/**
- * Created by wyq on 2018/6/18.
- */
+'use strict';
+
+const path = require('path');
+const fs = require('fs');
+const msgpack = require("msgpack-lite");
+const Uint64BE = require("int64-buffer").Uint64BE;
+
+const FPClient = require('./fpnn-sdk-nodejs/src/FPClient');
+
+let client = new FPClient({host: '182.254.147.76', port: 12321, autoReconnect: true, connectionTimeout: 10 * 1000});
+
+client.connect();
+
+
+client.on('connect', function () {
+	console.log("=========================== on client.connect: %j", +new Date());
+	/*let uid = +new Date();
+	let options = {
+		flag: 1,
+		method: 'iQuery',
+		payload: msgpack.encode({
+			"hintIds": [new Uint64BE(uid)],
+			"sql": "insert into user (`uid`, `nickname`, `regainTime`, `headFrame`) values (?, '?', '?', ?)",
+			"params": [uid.toString(), "1529487865", "2018-06-20 17:44:25", "160001"]
+		}),
+	};
+	console.log('================== uid: %j', uid);*/
+
+	let hintId = 1529489894425;
+	let options = {
+		flag: 1,
+		method: 'iQuery',
+		payload: msgpack.encode({
+			"hintIds": [new Uint64BE(hintId)],
+			"sql": "select * from user",
+		}),
+	};
+
+	client.sendQuest(options, function (data) {
+		console.log(data);
+		console.log(msgpack.decode(data.payload));
+	}, 10 * 1000);
+});
+
+client.on('error', function (err) {
+	console.error(err);
+});
--- LEMON-GRASS/dbproxy/fpnn-sdk-nodejs/test/test-fpnn.js
+++ LEMON-GRASS/dbproxy/fpnn-sdk-nodejs/test/test-fpnn.js
@@ -8,6 +8,7 @@ const FPClient = require('../src/FPClient');
 
 let client = new FPClient({ host: '35.167.185.139', port: 13013, autoReconnect: true, connectionTimeout: 10 * 1000 });
 
+client.connect();
 fs.readFile(path.resolve(__dirname, '../key/test-secp256k1-compressed-public.key'), function(err, data) {
 
     if (err) {
@@ -18,10 +19,10 @@ fs.readFile(path.resolve(__dirname, '../key/test-secp256k1-compressed-public.key
     client.encryptor('secp256k1', data, false, 128);
     client.connect(function(fpEncryptor) {
 
-        return msgpack.encode({ 
-            publicKey:fpEncryptor.pubKey, 
-            streamMode:fpEncryptor.streamMode, 
-            bits:fpEncryptor.strength 
+        return msgpack.encode({
+            publicKey:fpEncryptor.pubKey,
+            streamMode:fpEncryptor.streamMode,
+            bits:fpEncryptor.strength
         });
     });
 });
