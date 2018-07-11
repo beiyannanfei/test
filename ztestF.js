109b1173dbcedd79e18e6a6e945d35fa570e3e91
wyq <18810776836@163.com>
2018-07-11 15:28:48 +0800

    update

--- ztestF.js
+++ ztestF.js
@@ -1,2 +1,8 @@
-let str = "abc";
-console.log(str.length);
+let str = "[{\"title\":\"开服好礼！\",\"content\":\"HI~~小天使们大家好~~欢迎来到《选择：恋世界》测试服~为期3天的内测开始啦~\\n这里特别给亲爱的你准备了3000钻石！随便花~~你开心就好~~吼~~\",\"fromWho\":\"系统邮件\",\"itemList\":\"111_3000\",\"mailType\":1}]";
+
+str = JSON.parse(str);
+
+console.log(str);
+
+
+
